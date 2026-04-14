b4512fd7552ccb38ba8050867bb1e7784f480dbe
Jose Carlos Lozano <jose.lozano.acevedo@gmail.com>
2026-04-13 22:22:32 -0700

    New API validation for the GetSeatsCount POM

--- pages/GetSeatsCount.js
+++ pages/GetSeatsCount.js
@@ -7,11 +7,14 @@ export class GetSeatsCount {
     }
 
     async seatsCount(eventName){
-        await expect(this.eventCards.first()).toBeVisible();
+        await this.page.waitForResponse(response =>
+            response.url().includes('/api/events') &&
+            response.status() === 200 &&
+            response.request().method() === 'GET'
+        );
 
         const myEventCard = this.eventCards.filter({ hasText: eventName });
         await expect(myEventCard).toBeVisible();
-        await this.page.waitForTimeout(800); // I have to update this to not use a manual wait but rather expect an API answer
 
         const seatsText = await myEventCard.locator('span').last().textContent();
         return parseInt(seatsText.match(/\d+/)[0]);
